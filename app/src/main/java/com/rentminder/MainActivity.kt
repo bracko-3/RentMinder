edb833ab2d9fada2244122a64297abbb3c72d811
Wilmer 19 <wilmerxp19@gmail.com>
2023-02-22 20:10:41 -0500

    Adding spacer

--- app/src/main/java/com/rentminder/MainActivity.kt
+++ app/src/main/java/com/rentminder/MainActivity.kt
@@ -179,6 +179,7 @@ fun EditBillAmounts() {
                 ) {
                     Text(text = "Save & Remind")
                 }
+                Spacer(modifier = Modifier.height(10.dp))
         }
     }
 }
