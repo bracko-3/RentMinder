c7f8084c9df9ffb0a14ff0a09ceaedb08094e4ca
ItzSebas10 <77468585+ItzSebas10@users.noreply.github.com>
2023-03-22 15:54:07 -0400

    Created unit test for Household

--- app/src/test/java/com/rentminder/HouseholdUnitTest.kt
+++ app/src/test/java/com/rentminder/HouseholdUnitTest.kt
@@ -1,5 +1,14 @@
 package com.rentminder
 
-class HouseholdUnitTest {
+import com.rentminder.dto.Household
+import org.junit.Assert
+import org.junit.Test
 
+class HouseholdUnitTest {
+    @Test
+    fun `given a household dto when name is Uptown and members are 5 then name is Uptown and members are five`(){
+        var household = Household(1, "Uptown", 5)
+        Assert.assertTrue(household.houseName.equals("Uptown"))
+        Assert.assertTrue(household.houseMembers.equals(5))
+    }
 }
