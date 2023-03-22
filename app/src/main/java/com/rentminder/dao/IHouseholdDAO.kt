7cfc7f97ae15ce63c92ee7b6e5931c41f367c27f
ItzSebas10 <77468585+ItzSebas10@users.noreply.github.com>
2023-03-22 16:31:18 -0400

    IHouseholdDAO edited

--- app/src/main/java/com/rentminder/dao/IHouseholdDAO.kt
+++ app/src/main/java/com/rentminder/dao/IHouseholdDAO.kt
@@ -1,5 +1,10 @@
 package com.rentminder.dao
 
-interface IHouseholdDAO {
+import com.rentminder.dto.Household
+import retrofit2.Call
+import retrofit2.http.GET
 
+interface IHouseholdDAO {
+    @GET("")
+    fun getAllCountries() : Call<ArrayList<Household>>
 }
