8319a56257eca38aa512f6ddb2a399db291fd199
ItzSebas10 <77468585+ItzSebas10@users.noreply.github.com>
2023-03-22 15:43:18 -0400

    Added toString function to household DTO

--- app/src/main/java/com/rentminder/dto/Household.kt
+++ app/src/main/java/com/rentminder/dto/Household.kt
@@ -1,5 +1,7 @@
 package com.rentminder.dto
 
 class Household (var householdId : Int = 0, var houseName : String, var houseMembers : Int){
-
+    override fun toString(): String {
+        return "House name: $houseName Members: $houseMembers"
+    }
 }
