1c144fd3e6823acdd66242d48ca1d0cf99c0457c
Subu <SUBRAMANIYAN_ANNAMALAI@hotmail.com>
2023-03-21 22:28:02 -0400

    updated dto to include the various variables for payment data class and added string function to represent it

--- app/src/main/java/com/rentminder/DTO/Payment.kt
+++ app/src/main/java/com/rentminder/DTO/Payment.kt
@@ -1,4 +1,9 @@
 package com.rentminder.DTO
 
-class Payment {
+data class Payment (var paymentId: Int, var memberId: Int,var paymentType: String, var paymentNum: Int){
+
+    override fun toString(): String {
+        return "$paymentNum - $memberId"
+    }
+
 }
