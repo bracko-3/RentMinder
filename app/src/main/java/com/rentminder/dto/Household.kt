a8c7a32b07319f479f3d823c2bde9daec43dfb4c
ItzSebas10 <77468585+ItzSebas10@users.noreply.github.com>
2023-03-23 19:14:57 -0400

    Fixed DTO

--- app/src/main/java/com/rentminder/dto/Household.kt
+++ app/src/main/java/com/rentminder/dto/Household.kt
@@ -1,6 +1,6 @@
 package com.rentminder.dto
 
-class Household (var householdId : Int = 0, var houseName : String, var houseMembers : Int){
+data class Household (var householdId : Int = 0, var houseName : String, var houseMembers : Int){
     override fun toString(): String {
         return "$houseName $houseMembers Members"
     }
