2da5e13b1a9f040b13963f950b20eff0a2adb97c
Wilmer 19 <wilmerxp19@gmail.com>
2023-03-24 17:17:17 -0400

    Updated Members dto and created MembersUnitTest.kt

--- app/src/main/java/com/rentminder/dto/Members.kt
+++ app/src/main/java/com/rentminder/dto/Members.kt
@@ -2,6 +2,6 @@ package com.rentminder.dto
 
 class Members (var membersId : Int = 0, var memberName : String) {
     override fun toString(): String {
-        return "$memberName"
+        return "$membersId + $memberName"
     }
 }
