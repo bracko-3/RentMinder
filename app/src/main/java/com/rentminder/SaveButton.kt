824a07071079b8004eaeea515fa049a4a9b47d75
patel6rv <patel6rv@mail.uc.edu>
2023-03-28 18:35:14 -0400

    switch count to save with mutableStateOf false and add try catch block to click event

--- app/src/main/java/com/rentminder/SaveButton.kt
+++ app/src/main/java/com/rentminder/SaveButton.kt
@@ -1,5 +1,6 @@
 package com.rentminder
 
+import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
@@ -20,33 +21,38 @@ import androidx.compose.ui.unit.dp
 fun SaveRemindButton(enabled: Boolean, inputEdited: MutableState<Boolean>) {
     val context = LocalContext.current
     val focusManager = LocalFocusManager.current
-    var count by remember { mutableStateOf(0) }
+    var saved by remember { mutableStateOf(false) }
 
     IconButton(
         onClick = {
-            if (enabled) {
-                if (count > 0) {
-                    Toast.makeText(context, R.string.edit_saved_message, Toast.LENGTH_SHORT).show()
+            try{
+                if (enabled) {
+                    if (saved) {
+                        Toast.makeText(context, R.string.edit_saved_message, Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(context, R.string.saved_message, Toast.LENGTH_SHORT).show()
+                    }
+                    saved = true
+                    inputEdited.value = false
+                    focusManager.clearFocus()
                 } else {
-                    Toast.makeText(context, R.string.saved_message, Toast.LENGTH_SHORT).show()
+                    Toast.makeText(
+                        context,
+                        R.string.enter_value_message,
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
-                count++
-                inputEdited.value = false
-                focusManager.clearFocus()
-            } else {
-                Toast.makeText(
-                    context,
-                    R.string.enter_value_message,
-                    Toast.LENGTH_SHORT
-                ).show()
+            } catch(e: Exception){
+                Log.e("BtnClickEvent", "Error while handling click event", e)
             }
+
         },
     ) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Center
         ) {
-            if (count > 0 && enabled && !inputEdited.value) {
+            if (saved && enabled && !inputEdited.value) {
                 Icon(
                     painterResource(id = R.drawable.outline_check_circle_outline_24),
                     contentDescription = "Saved Icon",
