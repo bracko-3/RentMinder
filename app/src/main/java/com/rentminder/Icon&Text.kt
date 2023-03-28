e1d732880527e2668c55562a06028a4087ce48cd
patel6rv <patel6rv@mail.uc.edu>
2023-03-28 11:23:04 -0400

    Remove unused imports

--- app/src/main/java/com/rentminder/Icon&Text.kt
+++ app/src/main/java/com/rentminder/Icon&Text.kt
@@ -1,22 +1,16 @@
 package com.rentminder
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Home
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.style.TextDecoration
 
 @Composable
 fun RentIconText() {
