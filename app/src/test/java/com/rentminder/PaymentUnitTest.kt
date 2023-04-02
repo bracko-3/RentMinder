d4cb04f35317da8482649d4d39e76f0dd4ea34f7
grantkollar <89742140+grantkollar@users.noreply.github.com>
2023-04-02 13:22:37 -0400

    Rewording unit test to be more concise

--- app/src/test/java/com/rentminder/PaymentUnitTest.kt
+++ app/src/test/java/com/rentminder/PaymentUnitTest.kt
@@ -5,7 +5,7 @@ import org.junit.Test
 
 class PaymentUnitTest {
     @Test
-    fun `given a payment dto when payment type is credit and payment num is 200 then payment type is credit and payment num is 200`(){
+    fun `Given a payment when I select credit and input a value of 200 I should receive a payment of type credit with a value of 200`(){
         var payment = Payment(0,1,"credit",200.0)
         Assert.assertTrue(payment.paymentType.equals("credit"))
         Assert.assertTrue(payment.paymentNum.equals(200.0))
