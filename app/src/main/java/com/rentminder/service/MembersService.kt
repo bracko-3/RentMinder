4d735b088e7cc7c0c8597c57fcea0abe852d808d
Rydur01 <117382074+Rydur01@users.noreply.github.com>
2023-04-02 17:55:58 -0400

    implemented lazy to MemberService
    I implemented lazy to initialize the retrofitClientInstance, so it only gets initialized once, instead of everytime fetchMembersList() is called. Additionally, I added a try catch block to the suspend function to catch any exceptions, and prevent crashing by returning an emptyList() instead

--- app/src/main/java/com/rentminder/service/MembersService.kt
+++ app/src/main/java/com/rentminder/service/MembersService.kt
@@ -9,11 +9,18 @@ import kotlinx.coroutines.withContext
 import retrofit2.awaitResponse
 
 class MembersService {
-    suspend fun fetchMembersList() : List<Members> {
-        return withContext(Dispatchers.IO){
-            val service = RetrofitClientInstance.retrofitInstance?.create(MembersDAO::class.java)
-            val members = async {service?.getAllMembers()?.awaitResponse()?.body() }
-            return@withContext members.await()?: emptyList()
+    private val service: MembersDAO by lazy {
+        RetrofitClientInstance.retrofitInstance?.create(MembersDAO::class.java)
+            ?: throw IllegalStateException("RetrofitClientInstance is null")
+    }
+
+    suspend fun fetchMembersList(): List<Members> {
+        return withContext(Dispatchers.IO) {
+            try {
+                service.getAllMembers().awaitResponse().body() ?: emptyList()
+            } catch (e: Exception) {
+                emptyList()
+            }
         }
     }
 }
