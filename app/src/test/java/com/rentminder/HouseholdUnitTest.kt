d60f0d959b8a6f8cd81a1e1ad4273984d12cc55d
JakobBrownie <brown5jb@mail.uc.edu>
2023-04-02 11:38:25 -0400

    Removed redundant qualifier name, replaced unnecessary wording with binary operator to reduce clutter, removed unused import after previous update and updated var to val

--- app/src/test/java/com/rentminder/HouseholdUnitTest.kt
+++ app/src/test/java/com/rentminder/HouseholdUnitTest.kt
@@ -1,21 +1,20 @@
 package com.rentminder
 
 import com.rentminder.dto.Household
-import org.junit.Assert
 import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class HouseholdUnitTest {
     @Test
     fun `given a household dto when name is Uptown and members are 5 then name is Uptown and members are five`(){
-        var household = Household(1, "Uptown", 5)
-        Assert.assertTrue(household.houseName.equals("Uptown"))
-        Assert.assertTrue(household.houseMembers.equals(5))
+        val household = Household(1, "Uptown", 5)
+        assertTrue(household.houseName == "Uptown")
+        assertTrue(household.houseMembers == 5)
     }
 
     @Test
     fun `given a country dto when name is Uptown and members are 5 then output is Uptown 5 Members`() {
-        var household = Household(1, "Uptown", 5)
-        assertTrue(household.toString().equals("Uptown 5 Members"))
+        val household = Household(1, "Uptown", 5)
+        assertTrue(household.toString() == "Uptown 5 Members")
     }
 }
