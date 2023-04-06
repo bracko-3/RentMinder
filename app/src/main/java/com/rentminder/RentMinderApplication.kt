f967a2ffd5e16fe4c76952230b576f77df63236f
ItzSebas10 <77468585+ItzSebas10@users.noreply.github.com>
2023-04-06 11:11:30 -0400

    Updated build.gradle and added RentMinderApplication

---
+++ app/src/main/java/com/rentminder/RentMinderApplication.kt
@@ -0,0 +1,18 @@
+package com.rentminder
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.GlobalContext
+import org.koin.core.logger.Level
+
+class RentMinderApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        GlobalContext.startKoin {
+            androidLogger(if(BuildConfig.DEBUG) Level.ERROR else Level.NONE)
+            androidContext(this@RentMinderApplication)
+        }
+    }
+}
