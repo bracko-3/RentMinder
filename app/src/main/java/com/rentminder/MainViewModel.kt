bc32c59755a56ee88714b8853b6733ae3e6f3476
ItzSebas10 <77468585+ItzSebas10@users.noreply.github.com>
2023-04-06 11:02:39 -0400

    Edit to MainViewModel and moved PaymentService to Service package

--- app/src/main/java/com/rentminder/MainViewModel.kt
+++ app/src/main/java/com/rentminder/MainViewModel.kt
@@ -1,4 +1,11 @@
 package com.rentminder
 
+import androidx.lifecycle.MutableLiveData
+import com.rentminder.dto.Payment
+
 class MainViewModel {
+    var payments : MutableLiveData<List<Payment>> = MutableLiveData()
+    var paymentService : PaymentService = PaymentService()
+
+    
 }
--- app/src/main/java/com/rentminder/service/PaymentService.kt
+++ app/src/main/java/com/rentminder/service/PaymentService.kt
@@ -1,17 +1,18 @@
 package com.rentminder
+
 import com.rentminder.dao.PaymentDao
 import com.rentminder.dto.Payment
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.withContext
 import retrofit2.awaitResponse
+
 class PaymentService {
     suspend fun fetchPayment(): List<Payment>? {
         return withContext(Dispatchers.IO) {
             val service = RetrofitClientInstance.retrofitInstance?.create(PaymentDao::class.java)
             val payments = async { service?.getAllPayments() }
-            var result = payments.await()?.awaitResponse()?.body()
-            return@withContext result
+            return@withContext payments.await()?.awaitResponse<ArrayList<Payment>>()?.body()
         }
     }
 }
