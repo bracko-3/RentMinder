9b05911fb047c56dc2fb3aad557c9dca43bbebba
willblaker <willjblaker1@gmail.com>
2023-04-12 17:01:05 -0400

    - Created Members UI page

--- app/src/main/java/com/rentminder/MembersActivity.kt
+++ app/src/main/java/com/rentminder/MembersActivity.kt
@@ -4,18 +4,21 @@ import android.content.Intent
 import android.icu.text.SimpleDateFormat
 import android.icu.util.Calendar
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -31,23 +34,23 @@ class MembersActivity : ComponentActivity() {
                 Scaffold(topBar = {
                     TopAppBar(
                         title = {
-                            Text(text = "Top App Bar")
+                            Text(
+                                text = "RentMinder",
+                                fontSize = 25.sp,
+                                fontWeight = FontWeight.Bold,
+                            )
                         },
                         navigationIcon = {
                             IconButton(onClick = {
                                 startActivity(
                                     Intent(
-                                        this@MembersActivity,
-                                        MainActivity::class.java
+                                        this@MembersActivity, MainActivity::class.java
                                     )
                                 )
                             }) {
                                 Icon(Icons.Filled.ArrowBack, "backIcon")
                             }
                         },
-                        backgroundColor = MaterialTheme.colors.primary,
-                        contentColor = Color.White,
-                        elevation = 10.dp
                     )
                 }) { innerPadding ->
                     Column(
@@ -55,10 +58,12 @@ class MembersActivity : ComponentActivity() {
                             .fillMaxSize()
                             .padding(paddingValues = innerPadding)
                     ) {
-                        Text(
-                            text = "Content of the page", fontSize = 30.sp, color = Color.White
-                        )
-
+                        Surface(
+                            modifier = Modifier.fillMaxSize(),
+                            color = MaterialTheme.colors.background
+                        ) {
+                            MembersMenu()
+                        }
                     }
                 }
             }
@@ -66,9 +71,186 @@ class MembersActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun MembersMenu() {
+    val listItems = arrayOf("Person1", "Person2", "Person3", "Person4")
+    val contextForToast = LocalContext.current.applicationContext
 
+    // state of the menu
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+
+    // remember the selected item
+    var selectedItem by remember {
+        mutableStateOf(listItems[0])
+    }
+
+    Column() {
+        Row(
+            modifier = Modifier
+                .padding(
+                    top = 5.dp, bottom = 10.dp
+                )
+                .fillMaxWidth(), horizontalArrangement = Arrangement.Center
+        ) {
+            ExposedDropdownMenuBox(expanded = expanded, onExpandedChange = {
+                expanded = !expanded
+            }) {
+                // text field
+                TextField(
+                    value = selectedItem,
+                    onValueChange = {},
+                    readOnly = true,
+                    label = { Text(text = "Member") },
+                    trailingIcon = {
+                        ExposedDropdownMenuDefaults.TrailingIcon(
+                            expanded = expanded
+                        )
+                    },
+                    colors = ExposedDropdownMenuDefaults.textFieldColors()
+                )
+
+                // menu
+                ExposedDropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
+                    // this is a column scope
+                    // all the items are added vertically
+                    listItems.forEach { selectedOption ->
+                        // menu item
+                        DropdownMenuItem(onClick = {
+                            selectedItem = selectedOption
+                            expanded = false
+                        }) {
+                            Text(text = selectedOption)
+                        }
+                    }
+                }
+            }
+        }
+        membersPayments()
+    }
+}
+
+@Composable
+fun membersPayments() {
+    Column(
+        modifier = Modifier.padding(
+            horizontal = 15.dp
+        )
+    ) {
+        //Rent  text and button
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .padding(bottom = 10.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Rent", fontSize = 28.sp, fontWeight = FontWeight.Bold
+            )
+            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End) {
+                Button(
+                    onClick = {},
+                    shape = CutCornerShape(10),
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color.Red)
+                ) {
+                    Text(text = "Not Paid")
+                }
+            }
+        }
+
+        //Electric  text and button
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .padding(bottom = 10.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Text(
+                text = "Electric", fontSize = 28.sp, fontWeight = FontWeight.Bold
+            )
+            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End) {
+                Button(
+                    onClick = {},
+                    shape = CutCornerShape(10),
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color.Red)
+                ) {
+                    Text(text = "Not Paid")
+                }
+            }
+        }
+
+        //Water  text and button
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .padding(bottom = 10.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Text(
+                text = "Water", fontSize = 28.sp, fontWeight = FontWeight.Bold
+            )
+            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End) {
+                Button(
+                    onClick = {},
+                    shape = CutCornerShape(10),
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color.Red)
+                ) {
+                    Text(text = "Not Paid")
+                }
+            }
+
+
+        }
+
+        //Wifi  text and button
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .padding(bottom = 10.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Text(
+                text = "Wifi", fontSize = 28.sp, fontWeight = FontWeight.Bold
+            )
+            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End) {
+                Button(
+                    onClick = {},
+                    shape = CutCornerShape(10),
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color.Red)
+                ) {
+                    Text(text = "Not Paid")
+                }
+            }
+        }
+
+        //Other  text and button
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .padding(bottom = 10.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Text(
+                text = "Other", fontSize = 28.sp, fontWeight = FontWeight.Bold
+            )
+            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End) {
+                Button(
+                    onClick = {},
+                    shape = CutCornerShape(10),
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color.Red)
+                ) {
+                    Text(text = "Not Paid")
+                }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
