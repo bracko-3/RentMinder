72cfd824decabf7c6598f996b511ec6e8744406b
ItzSebas10 <77468585+ItzSebas10@users.noreply.github.com>
2023-04-19 23:56:05 -0400

    Implemented Code Review Suggestions

--- app/src/main/java/com/rentminder/dao/IPaymentDao.kt
+++ app/src/main/java/com/rentminder/dao/IPaymentDao.kt
@@ -1,14 +1,10 @@
 package com.rentminder.dao
 
-
-
 import com.rentminder.dto.Payment
 import retrofit2.Call
 import retrofit2.http.GET
 
-interface PaymentDao {
+interface IPaymentDao {
     @GET("")
     suspend fun  getAllPayments(): Call<ArrayList<Payment>>
-
-
 }
